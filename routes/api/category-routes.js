9cf193cbe1b9ea29802d23b9d11c38b69ef66de5
Christian Martinez <christian@mixdlabs.com>
2023-07-16 08:38:12 -0600

    Listing all categories when user lands on /api/categories. We have set a status code, success to be a boolean, text describing the response, and the data object where the data will reside in that object. Working as expected in insomnia get request to http://localhost:3001/api/categories/

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,28 @@ const { Category, Product } = require('../../models')
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  res.status(200).json('GET all categories')
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .json({ success: false, text: 'No categories found!', data: null })
+        return
+      } else {
+        res
+          .status(200)
+          .json({ success: true, text: 'Listing all categories', data: data })
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ success: false, error: err })
+    })
 })
 
 router.get('/:id', (req, res) => {
