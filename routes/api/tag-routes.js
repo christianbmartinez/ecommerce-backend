aedda2c4b633106556f5f7622aa149fb76314fac
Christian Martinez <christian@mixdlabs.com>
2023-07-16 16:01:41 -0600

    Added / tag route to show all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,28 @@ const { Tag, Product, ProductTag } = require('../../models')
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  res.status(200).json('GET all tags')
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id'],
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .json({ success: false, text: 'No tags found', data: null })
+        return
+      } else {
+        res
+          .status(200)
+          .json({ success: true, text: 'Listing all tags', data: data })
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ success: false, error: err })
+    })
 })
 
 router.get('/:id', (req, res) => {
