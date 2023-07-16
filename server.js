54923370406bca7ad1b6a5dd63eef2d9967a63aa
Christian Martinez <christian@mixdlabs.com>
2023-07-16 16:49:15 -0600

    Added middleware to view reqest methods in the console all color coded

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const routes = require('./routes')
+const { mw } = require('./middleware')
 // import sequelize connection
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+// Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(mw)
 app.use(routes)
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`)
+  console.log(`Server is listening on port ${PORT}`)
 })
