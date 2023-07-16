1add3ee936b9e4fd967f64b3d1148045f529d104
Christian Martinez <christian@mixdlabs.com>
2023-07-16 09:47:41 -0600

    Created a new category and filtering the category name. If it is not a string, or at least 2 characters, we will throw a descriptive error

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
       if (!data) {
         res
           .status(404)
-          .json({ success: false, text: 'No categories found!', data: null })
+          .json({ success: false, text: 'No categories found', data: null })
         return
       } else {
         res
@@ -67,8 +67,31 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  const { category_name } = req.body
+  if (typeof category_name !== 'string' || category_name.length < 2) {
+    res.status(400).json({
+      success: false,
+      text: `Please enter a valid category name. Expected a string of at least 2 characters, got ${category_name}`,
+      data: null,
+    })
+  } else {
+    Category.create({
+      category_name: category_name,
+    }).then((data) => {
+      res
+        .status(200)
+        .json({
+          success: true,
+          text: `Created category for ${category_name}`,
+          data: data,
+        })
+        .catch((err) => {
+          console.log(err)
+          res.status(500).json({ success: false, error: err })
+        })
+    })
+  }
 })
-
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 })
